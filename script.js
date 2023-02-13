db522a016d58044c3e76be59f5e2cf6dc8dd5e35
Poteau Florian <florianpoteau59@gmail.com>
2023-02-13 16:06:23 +0100

    création calculatrice console + alert

--- script.js
+++ script.js
@@ -1,33 +1,78 @@
-let p1 = document.getElementById('p1');
-let img = document.getElementById('img');
-let p2 = document.querySelector('.p2');
+alert("Bienvenue sur ce site unique");
 
-p1.innerHTML = 'HELLO WORLD';
-img.src = 'JavaScript-logo.png';
+let p1 = document.getElementById("p1");
+let img = document.getElementById("img");
+let p2 = document.querySelector(".p2");
 
-let bouton = document.querySelector('.submit');
+p1.innerHTML = "HELLO WORLD";
+img.src = "JavaScript-logo.png";
 
-bouton.addEventListener('click', event => {
-    bouton.innerHTML = `Nombre de clics : ${event.detail}`;
+let bouton = document.querySelector(".submit");
+
+bouton.addEventListener("click", (event) => {
+  bouton.innerHTML = `Nombre de clics : ${event.detail}`;
 });
 
-
-let img2 = document.querySelector('.img2');
+let img2 = document.querySelector(".img2");
 
 // cliquer.addEventListener('click', event => {
 //     img2.src = `${event.img}`;
 // });
 
-
 function afficher() {
-    img2.src = 'Javascript.png';
-};
+  img2.src = "Javascript.png";
+}
 
 function supprimer() {
-    img2.src = '';
-};
+  img2.src = "";
+}
 
+let x = 5;
+let y = "5";
 
+// If Else sur une ligne
 
+x==y ? console.log("x est bien égaux a y: " + true) : console.log(false);
 
+console.log("Pour utiliser la calculatrice: ")
+console.log("addition(x)");
+console.log("soustraction(x)");
+console.log("multiplication(x)");
+console.log("division(x)");
 
+let total = 0;
+
+function addition (x) {
+    total += x;
+    return total
+}
+
+function soustraction (x) {
+    total -= x;
+    return total
+}
+
+function multiplication (x) {
+    if (total === 0) {
+        return (total = x);
+    }
+else {
+    total *= x;
+    return total
+}
+}
+
+function division (x) {
+    if (total === 0) {
+        return (total = x)
+    }
+else {
+    total /= x;
+    return total
+}
+}
+
+function reset () {
+    total = 0;
+    return total
+}
