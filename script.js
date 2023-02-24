21de74b0baf676ec8094438f69f4394c90d061d8
Poteau Florian <florianpoteau59@gmail.com>
2023-02-24 12:06:37 +0100

    event scroll ajout nav et evenement au scroll

--- script.js
+++ script.js
@@ -309,6 +309,19 @@ else {
 }
 
 ring();
+})
 
+// Scroll Event
 
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+
+  if(window.scrollY > 120) {
+    nav.style.top = 0;
+  }
+  else{
+    nav.style.top = "-50px";
+  }
 })
