b7ff5651410b0046cec34c79c16187be69d956b8
Poteau Florian <florianpoteau59@gmail.com>
2023-03-01 15:26:10 +0100

    form event

--- script.js
+++ script.js
@@ -328,9 +328,12 @@ window.addEventListener("scroll", () => {
 
 const inputName = document.querySelector('input[type="text"]');
 const select = document.querySelector("select");
+const form = document.querySelector("form");
 
 let pseudo = "";
 let language = "";
+
+
 
 inputName.addEventListener('input', (e) =>{
 
@@ -340,8 +343,24 @@ inputName.addEventListener('input', (e) =>{
 
 select.addEventListener("input", (e) =>{
 
-console.log(e.target.value);
+  language = e.target.value;
 
-})
+});
+
+form.addEventListener("submit", (e) =>{
+  e.preventDefault();
+
+  console.log(cgv.checked);
+
+  if(cgv.checked) {
+    document.querySelector("form > div").innerHTML = `
+    <h3>Pseudo : ${pseudo}</h3>
+    <h4>Language préféré: ${language}</h4>
+    `
+  } else {
+    alert("Veuillez accepter les CGV")
+  }
+
+});
 
 
